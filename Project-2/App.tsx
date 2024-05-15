44896fbcd14c4369ae2bed3d253c67ca035b5c4a
Aman Fangeria <aman.fangeria@geekyants.com>
2024-05-15 17:11:07 +0530

    Created a contact list component and used map to iterate over the array and render elements

--- Project-2/App.tsx
+++ Project-2/App.tsx
@@ -5,6 +5,7 @@ import FlatCards from './components/FlatCards';
 import ElevatedCards from './components/ElevatedCards';
 import FancyCard from './components/FancyCard';
 import ActionCard from './components/ActionCard';
+import ContactList from './components/ContactList';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <FlatCards />
           <ElevatedCards />
           <FancyCard />
+          <ContactList />
           <ActionCard />
         </View>
       </ScrollView>
