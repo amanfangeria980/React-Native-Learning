ef5a9c7dd95c89e6fea800bb1bee41cafcff5c61
Aman Fangeria <aman.fangeria@geekyants.com>
2024-05-16 12:55:29 +0530

    created various states and wrote the business logic for generating password

--- Project-3-Password-Generator/PasswordGenerator/App.tsx
+++ Project-3-Password-Generator/PasswordGenerator/App.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import * as Yup from 'yup';
 
 const PasswordSchema = Yup.object().shape({
@@ -10,6 +10,45 @@ const PasswordSchema = Yup.object().shape({
 });
 
 const App = () => {
+  const [password, setPassword] = useState('');
+  const [isPassGenerated, setIsPassGenerated] = useState(false);
+  const [useLowerCase, setUseLowerCase] = useState(false);
+  const [useUpperCase, setUseUpperCase] = useState(false);
+  const [useNumbers, setUseNumbers] = useState(false);
+  const [useSymbols, setUseSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLength: number) => {
+    let characterList = '';
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const digitChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+';
+
+    if (useUpperCase) characterList += upperCaseChars;
+    if (useLowerCase) characterList += lowerCaseChars;
+    if (useNumbers) characterList += digitChars;
+    if (useSymbols) characterList += specialChars;
+
+    const passwordResult = createPassword(characterList, passwordLength);
+    setPassword(passwordResult);
+    setIsPassGenerated(true);
+  };
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
+  const resetPasswordState = () => {
+    setPassword('');
+    setIsPassGenerated(false);
+    setUseUpperCase(false);
+    setUseLowerCase(false);
+    setUseNumbers(false);
+    setUseSymbols(false);
+  };
   return (
     <View>
       <Text>App</Text>
